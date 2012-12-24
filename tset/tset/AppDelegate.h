932d63eebb034ded4d7f08a10a10d17992109a33
Detay <OmerFaruk@Detays-MacBook.local>
2012-12-24 17:59:46 +0200

    stset

---
+++ tset/tset/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tset
+//
+//  Created by Detay on 12/24/12.
+//  Copyright (c) 2012 DetaySoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tset/tset/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tset
+//
+//  Created by Detay on 12/24/12.
+//  Copyright (c) 2012 DetaySoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
